bcbef05ad50e33c648dba494d0052a4bc1225b42
paijeem <93543751+paijeem@users.noreply.github.com>
2023-01-09 00:06:40 +0700

    Create color_ch.js

---
+++ color_ch.js
@@ -0,0 +1,3 @@
+document.body.style.backgroundColor = "pink";
+document.getElementById("app").style.backgroundColor = "#0a0a0a";
+document.getElementsByClassName("certify").style.backgroundColor = "red";
